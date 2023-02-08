681bea884d4522b45ac627ccc2f6dd3a878c894a
David Locke <redwallknight@gmail.com>
2023-02-07 18:36:16 -0600

    Adding simple server for testing against

---
+++ example-server/server.py
@@ -0,0 +1,28 @@
+
+from configparser import ConfigParser
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+server_address = ('0.0.0.0', 8000)
+
+with open('example-server/example-data.txt', 'r') as f:
+    config = ConfigParser()
+    config.read_file(f)
+    print(config.sections())
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        route, query = self.path.split('?')
+        route = route.strip('/')
+        member_id = query[query.index('=') + 1:]
+        print(route, member_id)
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        self.wfile.write(config.get(member_id, route).encode())
+
+if __name__ == "__main__":
+    server = HTTPServer(server_address, RequestHandler)
+    server.serve_forever()
